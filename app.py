da0e1cd31fac8374271b2a434ab89f56d01fcd2f
Isabelle Mohr <isabelle.mohr@jina.ai>
2024-06-19 16:15:47 +0200

    use rankbm25

--- app.py
+++ app.py
@@ -5,23 +5,26 @@ import faiss
 import numpy as np
 import pickle
 from whoosh.index import open_dir
-from whoosh.qparser import QueryParser
 import os
+from rank_bm25 import BM25Okapi
 
-os.environ['KMP_DUPLICATE_LIB_OK']='True'
+os.environ['KMP_DUPLICATE_LIB_OK'] = 'True'
 
 app = Flask(__name__)
 
 # Load the model, index, and documents
-print("LOADING MODEL")
-model = AutoModel.from_pretrained('jinaai/jina-embeddings-v2-base-de', trust_remote_code=True)
-print("LOADING INDEX")
+model = AutoModel.from_pretrained(
+    'jinaai/jina-embeddings-v2-base-de', trust_remote_code=True
+)
 index = faiss.read_index('index.faiss')
 with open('documents.pkl', 'rb') as f:
     documents = pickle.load(f)
 
-ix = open_dir("whoosh_index")
-print("EVERYTHING LOADED")
+
+tokenized_corpus = [doc.split(' ') for doc in documents]
+bm25_model = BM25Okapi(tokenized_corpus)
+
+
 def normalize_bm25_scores(scores):
     normalized_scores = []
     for score in scores:
@@ -37,9 +40,14 @@ def weighted_sum(bm25_scores, vector_scores, weights={'bm25': 0.4, 'vector': 0.6
     min_bm25_score = min([x['score'] for x in bm25_scores], default=0)
     min_vector_score = min([x['score'] for x in vector_scores], default=0)
 
-    bm25_scores_dict = {result['id']: result['score'] for result in normalized_bm25_scores}
+    bm25_scores_dict = {
+        result['id']: result['score'] for result in normalized_bm25_scores
+    }
     vector_scores_dict = {result['id']: result['score'] for result in vector_scores}
-    all_ids = set([result['id'] for result in bm25_scores] + [result['id'] for result in vector_scores])
+    all_ids = set(
+        [result['id'] for result in bm25_scores]
+        + [result['id'] for result in vector_scores]
+    )
     for id in all_ids:
         if id not in bm25_scores_dict:
             bm25_scores_dict[id] = min_bm25_score
@@ -49,31 +57,40 @@ def weighted_sum(bm25_scores, vector_scores, weights={'bm25': 0.4, 'vector': 0.6
     # Weight and Sum
     combined_scores = []
     for id in all_ids:
-        combined_score = weights['bm25'] * bm25_scores_dict[id] + weights['vector'] * vector_scores_dict[id]
-        combined_scores.append({'score': combined_score, 'id': id, 'text': documents[id]})
+        combined_score = (weights['bm25'] * bm25_scores_dict[id]) + (
+            weights['vector'] * vector_scores_dict[id]
+        )
+        combined_scores.append(
+            {'score': combined_score, 'id': id, 'text': documents[id]}
+        )
 
     return combined_scores
 
 
-def bm25_search(query):
-    qp = QueryParser("content", schema=ix.schema)
-    q = qp.parse(query)
-    with ix.searcher() as searcher:
-        results = searcher.search(q, limit=20)  # Adjust limit as necessary
-        return [{'text': result['content'], 'score': result.score, 'id': result.docnum} for result in results]
+def bm25_search(query, limit=20):
+    tokenized_query = query.split(' ')
+    scores = bm25_model.get_scores(tokenized_query)
+    top_n = np.argsort(scores)[::-1][:limit]
+    return [{'text': documents[i], 'score': scores[i], 'id': i} for i in top_n]
+
 
 def hybrid_search(bm25_results, vector_results):
     # Combine scores using weighted sum
-    combined_scores = weighted_sum(bm25_results, vector_results) # [{'score': combined_score, 'id': doc_id}]
+    combined_scores = weighted_sum(
+        bm25_results, vector_results
+    )  # [{'score': combined_score, 'id': doc_id}]
     # sort the combined scores in descending order using lamda
-    sorted_combined_results = sorted(combined_scores, key=lambda x: x['score'], reverse=True)
-    print("Combined Results:", sorted_combined_results)
+    sorted_combined_results = sorted(
+        combined_scores, key=lambda x: x['score'], reverse=True
+    )
 
     return sorted_combined_results[:20]  # Limit to top 20 results
+
 
 @app.route('/')
 def home():
     return render_template('index.html')
+
 
 @app.route('/vector_search', methods=['POST'])
 def vector_search(query, k=20):
@@ -83,11 +100,17 @@ def vector_search(query, k=20):
     # Filter out invalid indices (-1) and corresponding distances
     valid_indices = [idx for idx in indices[0] if idx != -1]
     valid_vectors = [dist for dist, idx in zip(vectors[0], indices[0]) if idx != -1]
-    cosine_scores = [cosine_similarity(query_embedding, [vector]) for vector in valid_vectors]
+    cosine_scores = [
+        cosine_similarity(query_embedding, [vector]) for vector in valid_vectors
+    ]
     # Return results with valid indices and distances
-    results = [{'text': documents[idx], 'score': float(score), 'id': idx} for score, idx in zip(cosine_scores, valid_indices)]
+    results = [
+        {'text': documents[idx], 'score': float(score), 'id': idx}
+        for score, idx in zip(cosine_scores, valid_indices)
+    ]
 
     return results
+
 
 @app.route('/search', methods=['POST'])
 def search():
@@ -95,12 +118,22 @@ def search():
     bm25_results = bm25_search(query)
     vector_results = vector_search(query)
     hybrid_results = hybrid_search(bm25_results, vector_results)
-    display_results_bm25 = [{'text': result['text'], 'score': result['score']} for result in bm25_results]
-    display_results_vector = [{'text': result['text'], 'score': result['score']} for result in vector_results]
-    display_results_hybrid = [{'text': result['text'], 'score': result['score']} for result in hybrid_results]
+    display_results_bm25 = [
+        {'text': result['text'], 'score': result['score']} for result in bm25_results
+    ]
+    display_results_vector = [
+        {'text': result['text'], 'score': result['score']} for result in vector_results
+    ]
+    display_results_hybrid = [
+        {'text': result['text'], 'score': result['score']} for result in hybrid_results
+    ]
 
-    return jsonify(bm25=display_results_bm25, vector=display_results_vector, hybrid=display_results_hybrid)
+    return jsonify(
+        bm25=display_results_bm25,
+        vector=display_results_vector,
+        hybrid=display_results_hybrid,
+    )
+
 
 if __name__ == '__main__':
     app.run(debug=True)
-
